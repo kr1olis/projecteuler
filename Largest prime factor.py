4eef4ff3e925bee6792b41287eec08ddf4f24621
criminalorange <criminalorange@gmail.com>
2015-07-26 14:45:40 +0300

    testcommit2

--- Largest prime factor.py
+++ Largest prime factor.py
@@ -4,4 +4,16 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 
-
+number = 600851475143
+sqrtnumber = int(number ** 0.5)
+result = [x for x in range(sqrtnumber)]
+for i in range(2,sqrtnumber):
+    for j in range(2*i,sqrtnumber,i):
+        result[j] = 0
+primes = [x for x in result if result[x] != 0]
+primes.reverse()
+for i in range(len(primes)):
+    if number % primes[i] == 0:
+        answer = primes[i]
+        break
+print(answer)
